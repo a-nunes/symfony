56ea7beede92b1a9ce04b932c0b4acc090a2945b
Artur Nunes <art.nunes@gmail.com>
2019-11-08 15:18:59 -0300

    I'm going faster than I thought. It's a good thing.

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,14 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Michelf\Markdown;
+use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -21,7 +26,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug)
+    public function show($slug, MarkdownParserInterface $markdown)
     {
         $comments = [
             'I ate bacon at lunch',
@@ -29,10 +34,31 @@ class ArticleController extends AbstractController
             'I\'ll never eat every flavor beans again.'
         ];
 
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.
+EOF;
+
+        $articleContent = $markdown->transformMarkdown($articleContent);
+
         dump($slug, $this);
         return $this->render('article/show.html.twig',[
             'title' => ucwords(str_replace('-',' ',$slug)),
+            'articleContent' => $articleContent,
+            'slug' => $slug,
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        // TODO - actually heart/unheart the article!
+        $logger->info('Article is being hearted.');
+        return $this->json(['hearts' => rand(5,100)]);
     }
 }
